b52a79119f66257e9e6c4c17f1e8fc0c1fde9ce1
Rácz Tamás <racztamas12@gmail.com>
2024-10-16 09:20:41 +0200

    Uj valtozo comment

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,6 +14,7 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
+  //kapott egy új változót az osztály
   private Random generator = new Random();
 
   public TorpedoStore(int numberOfTorpedos){
